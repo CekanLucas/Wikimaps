903d816799ff88422394e4906a9178ad0ca73247
Lucas <cekanlucas@gmail.com>
2020-03-06 20:50:39 +0000

    removed ejs with extreme prejustice

--- server.js
+++ server.js
@@ -21,7 +21,6 @@ db.connect();
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
 app.use(morgan('dev'));
 
-app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/styles", sass({
   src: __dirname + "/styles",
@@ -47,7 +46,8 @@ app.use("/api/widgets", widgetsRoutes(db));
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
 app.get("/", (req, res) => {
-  res.render("index");
+  // res.render("index");
+  res.redirect('https://mapwiki.netlify.com/')
 });
 
 app.listen(PORT, () => {
