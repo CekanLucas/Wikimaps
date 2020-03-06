3afe080cd912e60ee016be4d6dff34f4d335c90d
Lucas <cekanlucas@gmail.com>
2020-03-06 21:16:29 +0000

    move files to public

--- server.js
+++ server.js
@@ -46,8 +46,8 @@ app.use("/api/widgets", widgetsRoutes(db));
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
 app.get("/", (req, res) => {
-  // res.render("index");
-  res.redirect('https://mapwiki.netlify.com/')
+  res.render("index.html");
+  res.redirect('/index.html')
 });
 
 app.listen(PORT, () => {
