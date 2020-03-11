ee34c8ff9bee24e6f043cbea136f033d8783d6b2
Lucas <cekanlucas@gmail.com>
2020-03-11 18:38:05 +0000

    random colours to style elements and proper padding to list buttons

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,22 +5,22 @@ $(document).ready(function() {
     let count = 3;
     for (let map of users) {
       $("#map-container").append(`
-          <article class=map-grid>
-            <div id="${map.id}" class='map map-item' style='width: 100%; height: 100%;'></div>
-            <h3 class=map-name title='view map'>${map.title}</h3>
+      <article class=map-grid>
+      <div id="${map.id}" class='map map-item' style='width: 100%; height: 100%;'></div>
+      <h3 class=map-name title='view map'>${map.title}</h3>
             <span class=user-handle title="go to user's maps"> ${map.email} <i class="fas fa-atlas"></i></span><br>
             <a class=map-option href="http://" title='Favourite Map'><i class="fas fa-heart"></i></a>
             <a class=map-option href="http://" title='Delete Map'><i class="fas fa-times"></i></a>
             <a class=map-option href="http://" title='Add pins from Map'><i class="fas fa-map-marked"></i></a>
             <a class=map-option href="http://" title='Remove Pins from Map'><i class="far fa-map"></i></a>
             <a class=map-option href="http://" title='Edit Map'><i class="fas fa-edit"></i></a>
-          </article>
-    `);
-      count++;
-    }
-  };
+            </article>
+            `)
+            count++;
+          }
+        };
 
-  $.ajax({
+        $.ajax({
     method: "GET",
     url: "/api/users"
   }).then(users => {
@@ -30,7 +30,9 @@ $(document).ready(function() {
       window.maps[map.id] = {
         markers: []
       };
+      let randHue = Math.random() * 360;
       mapMaker(map.id);
+      $(`#${map.id}`).css('filter', `invert(1) sepia(0.8) hue-rotate(${randHue}deg)`);
     }
   });
 });
--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,5 +1,7 @@
 $(document).ready(  () => {
   console.log('Form Ready')
+
+
   $('#login-button').click( e => {
     console.log('Login button click')
     $('#input-form').trigger('submit');
@@ -10,21 +12,100 @@ $('#input-form').submit(
   e => {
     e.preventDefault();
     console.log('submitinger')
-    $.ajax({
+     $.ajax({
       type: 'POST',
       url: '/login',
     })
-    .then( res => {
+    .then( (res, a, b) => {
       console.log('Response')
       console.log([res])
+      console.log(a, b)
+      return;
+    })
+    .catch(e => {
+      console.log('error')
+      console.log(e)
+
+      return;
     })
   }
 )
 
+//works bring user specific map back and front
+$('#map').css('z-index',5).css('filter','invert(1)')
+$('#map').css('z-index',-1000).css('filter','invert(1) blur(1.1px)')
 
-
-  $('form-msg').click( () => {
-    console.log('clicked')
-    $('#form-msg').css('color','red');
-  }  )
 });
+
+
+/* // ------------STATE 1---------------- ask for email
+// -- form msg --
+        <b><span id='form-msg text-light'>Enter Email:&nbsp;&nbsp;&nbsp;&nbsp;</span></b>
+
+// -- input form --
+    <form
+      class='form-control form-control-lg form-inline'
+      method=post action='/login'id='input-form'
+      style="display:contents !important" name=usernameform>
+        <input class=form-control name=login-email type="email" placeholder="example@email.com">
+      </form>
+      <button class='btn btn-warning' id='login-button'>Login</button>
+
+// -- register --
+    <form
+      class='form-control form-control-lg form-inline'
+      action="/register" method=GET
+      style="display:contents !important" name=usernameform>
+        <button class='btn btn-warning' id=register-button>Register</button>
+    </form>
+
+// ------------STATE 2---------------- ask for pass
+// -- form msg --
+ Enter Password for&nbsp;<b><span class=text-light><%= users[userID].email %>&nbsp;&nbsp;&nbsp;</span></b></b>
+
+        <b><span id='form-msg text-light'>Enter Email:&nbsp;&nbsp;&nbsp;&nbsp;</span></b>
+
+// -- input form --
+    <form
+      class='form-control form-control-lg form-inline'
+      method=post action='/login'id='input-form'
+      style="display:contents !important" name=usernameform>
+        <input class=form-control name=login-email type="email" placeholder="example@email.com">
+      </form>
+      <button class='btn btn-warning' id='login-button'>Login</button>
+
+// -- register --
+    <form
+      class='form-control form-control-lg form-inline'
+      action="/register" method=GET
+      style="display:contents !important" name=usernameform>
+        <button class='btn btn-warning' id=register-button>Register</button>
+    </form>
+
+
+ // ------------STATE 2---------------- Logged in as user
+          Logged in for:&nbsp;<b><span class=text-light><%= users[userID].email %>&nbsp;&nbsp;</span</b>
+      <form
+      class='form-control form-control-lg form-inline'
+      action="/logout" method=POST id=LoginForm
+      style="display:contents !important" name=usernameform>
+        <button class='btn btn-warning'>Logout</button>
+    </form> */
+
+// ----- NOTES FOR LUCAS
+//     $('#input-form').html()
+// "
+//         <input class=\"form-control\" name=\"login-email\" type=\"email\" placeholder=\"example@email.com\">
+//       "
+// $('#input-form input').val()
+// "cekancelucas@gmail.com"
+// $('#input-form input').before('<i>Hello<i>')
+// Object { 0: input.form-control
+// , length: 1, prevObject: {…} }
+
+// $('#input-form').before('<i>Hello<i>')
+// Object { 0: form#input-form.form-control.form-control-lg.form-inline, length: 1 }
+/*
+<input type="color" id="head" name="head"
+           value="#e66465"></input> */
+// https://developers.google.com/maps/documentation/javascript/styling
