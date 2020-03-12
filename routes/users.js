b6b8bc36987080ca8a51260176504db1a77255ec
Amino Asmaa Ali <amino.asmaa.ali@gmail.com>
2020-03-12 21:35:43 +0000

    favourite maps

--- routes/users.js
+++ routes/users.js
@@ -8,22 +8,6 @@
 const express = require("express");
 const router = express.Router();
 
-const createMapElement = function(mapsObject) {
-  for (let map in mapsObject) {
-    $("#map-container").append(`
-    <div id="map2" class='map map-item' style='width: 100%; height: 100%;'></div>
-    <h3 class=map-name title='view map'> ${map.email}</h3>
-    <span class=user-handle title="go to user's maps"> ${map.title} <i class="fas fa-atlas"></i></span><br>
-    <a class=map-option href="http://" title='Favourite Map'><i class="fas fa-heart"></i></a>
-    <a class=map-option href="http://" title='Delete Map'><i class="fas fa-times"></i></a>
-    <a class=map-option href="http://" title='Add pins from Map'><i class="fas fa-map-marked"></i></a>
-    <a class=map-option href="http://" title='Remove Pins from Map'><i class="far fa-map"></i></a>
-    <a class=map-option href="http://" title='Edit Map'><i class="fas fa-edit"></i></a>
-    </article>
-  `);
-  }
-};
-
 module.exports = db => {
   router.get("/", (req, res) => {
     db.query(
