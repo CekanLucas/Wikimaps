0b24cce18738843b01f44f166e27065207bf74e9
Lucas <cekanlucas@gmail.com>
2020-03-13 00:31:25 +0000

    style and render error free

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -90,6 +90,7 @@ $(document).ready(() => {
       $("#form-msg").text("Enter Email:  ");
       $("#register-button").show(500);
       $(".nav-item").hide(500);
+      $("#login-button").hide(500);
       return;
     });
   });
